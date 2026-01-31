77451c415c910780c09e8561edecdaf6e62cbd52
benewend2 <osandrine227@gmail.com>
2026-01-31 11:58:34 +0000

    Fix: connect frontend to Render backend

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # CORS pour React (ajoute l'URL de ton frontend Vercel)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ton-frontend.vercel.app"],  # remplace par ton URL Vercel
+    allow_origins=["mental-chatbot-frontend-seven.vercel.app"],  # remplace par ton URL Vercel
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
